35d2cc652ec63230aa7a0cb038d08526e2af0c45
Dimitrios Siganos <dimitris@siganos.org>
2019-03-16 16:00:47 +0000

    unix2dos

--- provision_stage2.bat
+++ provision_stage2.bat
@@ -1,3 +1,4 @@
+echo privision_stage2.bat starting
 wget --no-check-certificate -O gcc-arm-none-eabi-8-2018-q4-major-win32.zip https://developer.arm.com/-/media/Files/downloads/gnu-rm/8-2018q4/gcc-arm-none-eabi-8-2018-q4-major-win32.zip?revision=ab9cb8f8-6a9d-4a6e-818a-295f5d1ca982?product=GNU%20Arm%20Embedded%20Toolchain,ZIP,,Windows,8-2018-q4-major
 7z -O C:\gnu_arm_embedded x gcc-arm-none-eabi-8-2018-q4-major-win32.zip
 setx ZEPHYR_TOOLCHAIN_VARIANT gnuarmemb
@@ -6,3 +7,4 @@ pip2 install west
 west init zephyrproject
 cd zephyrproject
 west update
+echo privision_stage2.bat done
