321526f8bdc04d9bda9089fe6387130ded4e94ca
Dimitrios Siganos <dimitris@siganos.org>
2019-03-18 17:46:05 +0000

    Install pyelftools

--- provision_stage1.ps1
+++ provision_stage1.ps1
@@ -2,4 +2,4 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 
 choco feature enable -n allowGlobalConfirmation
 choco install cmake --installargs 'ADD_CMAKE_TO_PATH=System'
-choco install git python ninja dtc-msys2 gperf wget 7zip
+choco install git python ninja dtc-msys2 gperf wget 7zip pyelftools
